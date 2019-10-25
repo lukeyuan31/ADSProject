967696873c1ba7efa6769a990cb260c6231b8881
lukeyuan31 <1393499408@qq.com>
2019-10-25 13:31:04 -0400

    Initial commit

---
+++ src/min_heap.java
@@ -0,0 +1,35 @@
+class building{
+        private int buildingNum;
+        private int executed_time;
+        private int total_time;
+
+        public int getBuildingNum() {
+            return buildingNum;
+        }
+
+        public int getExecuted_time() {
+            return executed_time;
+        }
+
+        public int getTotal_time() {
+            return total_time;
+        }
+
+        public void setBuildingNum(int buildingNum) {
+            this.buildingNum = buildingNum;
+        }
+
+        public void setExecuted_time(int executed_time) {
+            this.executed_time = executed_time;
+        }
+
+        public void setTotal_time(int total_time) {
+            this.total_time = total_time;
+        }
+    }
+
+
+    public class min_heap {
+
+
+}
